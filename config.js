1f802fd4c3c7c5a46cb7ca6943e478eadd4f6e19
John Cockrell <jcockrell@gmail.com>
2018-10-14 16:48:15 +0100

    Remove old keys

--- config.js
+++ config.js
@@ -5,8 +5,8 @@
  access_token_secret
  */
 module.exports = {
-    consumer_key: 'Hbii2GYcuabGVz6fibfzAet3v',
-    consumer_secret: 'MvxA5xIMdYkP06IRibGvcjhHpLDeyTbqM8F58YxV4WGhFd10cT',
-    access_token: '1051421613286154240-4QbEExE8CUR0EFBd7bP5ArxKBp5WuL',
-    access_token_secret: 'QqjUArtFiV0XbLdGKgp8XfNtbRufGk1KXPlszZVTBgJ0c'
+    consumer_key: 'xxxx',
+    consumer_secret: 'xxxx',
+    access_token: 'xxxx',
+    access_token_secret: 'xxxx'
    }
